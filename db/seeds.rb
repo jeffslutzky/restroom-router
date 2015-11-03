1e2e27d18c0a6ed9bfb848b57958fedb9a960b91
jaygr1 <jrib6@yahoo.com>
2015-11-03 17:43:46 -0500

    Pushing most recent, no major changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,36 @@
 PublicPark.delete_all
 
 Adapters::RestroomParser.formatted_data.each do |data|    #iterate through each park (hash)
-    sleep(0.11)
+    sleep(0.11) #Geocoder gem only allows for 10 queries per second
     new_park = PublicPark.new(data)  
       name = new_park.name  
       location = new_park.location  
         latlng = Geocoder.search(location)  #use Geocoder gem to get geo information
         #latlng = latlng.first['geometry']['location']  #parse Geocoder gem output to get latitude and longitude 
-
+        boroughs = ["Bronx County", "Kings County", "New York County", "Queens County", "Richmond County"]
         unless latlng.count < 1
+          
     
           s_results = RecursiveOpenStruct.new(latlng.first.geometry['location'], :recurse_over_arrays => true)
+            
+          location_check = latlng.first.address_components.any? do |test|
+             #test.values.include?("Bronx County", "Kings County", "New York County", "Queens County", "Richmond County")
           
+             boroughs.each do |borough|
+            test.has_value?(borough)
+          end
+           
+           end
+             if location_check
 
-          new_park.latitude = s_results.lat
-          new_park.longitude = s_results.lng
-          new_park.save
+            new_park.latitude = s_results.lat
+            new_park.longitude = s_results.lng
+            new_park.save
+          
         end
+      end
   end
   
+
+
+  #[5]["short_name"]
