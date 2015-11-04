63b35e10b3e17369889f9894ceab5f1288dc1ba5
hwinn4 <hilary.winn@gmail.com>
2015-11-04 13:24:32 -0500

    adds review model and average rating method

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103144202) do
+ActiveRecord::Schema.define(version: 20151104175908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20151103144202) do
     t.datetime "updated_at",          null: false
     t.float    "latitude"
     t.float    "longitude"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.integer  "user_id"
+    t.string   "comment"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "public_park_id"
   end
 
 end
