f3fbb9fb2b3c0b6286f78b6eca6102eaf3c74f2c
hwinn4 <hilary.winn@gmail.com>
2015-11-04 18:04:09 -0500

    change review view object iteration from first 5 to last 5

--- app/models/rating_view_object.rb
+++ app/models/rating_view_object.rb
@@ -1,13 +1,12 @@
 class RatingViewObject
 
-
   def initialize(park)
     @park = park 
   end
 
   def display_top_five_comments
     if !(@park.reviews.empty?)
-      @park.reviews.first(5).map do |review|
+      @park.reviews.last(5).map do |review|
         review.comment
       end
     else
