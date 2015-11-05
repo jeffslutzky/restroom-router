a5aa1640e5b64b83716fadaa001f7cede5b5c929
jaygr1 <jrib6@yahoo.com>
2015-11-05 13:01:08 -0500

    User can review existing location

--- app/controllers/public_parks_controller.rb
+++ app/controllers/public_parks_controller.rb
@@ -5,5 +5,16 @@ class PublicParksController < ApplicationController
     @hash = {lat: PublicPark.first.latitude, lng: PublicPark.first.longitude}
   end
 
+  def show
+    @public_park = PublicPark.find(params[:id])
+    @review = @public_park.reviews.build
+  end
+
+  def create
+
+  end
+
+
+
 
 end
